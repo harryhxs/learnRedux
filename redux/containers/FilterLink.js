c90714a97f0c65226c47a240f18bb2a652892712
harryhxs <317428544@qq.com>
2017-08-21 11:01:16 +0800

    Update FilterLink.js

--- redux/containers/FilterLink.js
+++ redux/containers/FilterLink.js
@@ -4,7 +4,7 @@ import Link from "../components/Link"
 
 const mapStateToProps=(state,ownProps)=>{
     return {
-        active:ownProps.filter===state.visiblityFilter
+        active:ownProps.filter===state.visibilityFilter
     }
 };
 const mapDispatchToProps=(dispatch, ownProps) =>{
